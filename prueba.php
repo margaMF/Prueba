63e28fc9ed05b458e685670abd0edb749130f054
margaMF <102173684+margaMF@users.noreply.github.com>
2023-10-16 12:53:59 +0200

    Add files via upload

---
+++ prueba.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+<body>
+    <h1>PRUEBA</h1>
+    <p>Esto es una pureba para git</p>
+    <p>Pureba para un commit</p>
+    <p>otro</p>
+</body>
+</html>
